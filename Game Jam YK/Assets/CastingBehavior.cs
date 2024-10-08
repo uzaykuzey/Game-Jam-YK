ed370a33ab39942d3ba089aec6b30315412990c2
Uzay Kuzey <atauzayk@gmail.com>
2024-10-08 17:23:23 +0300

    new boss attack

--- Game Jam YK/Assets/CastingBehavior.cs
+++ Game Jam YK/Assets/CastingBehavior.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CastingBehavior : StateMachineBehaviour
 {
-    public static Color purple = new Color(0.7133158f, 0.211819f, 0.990566f, 1);
     public float looking;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -14,7 +13,7 @@ public class CastingBehavior : StateMachineBehaviour
         Controller.instance.bossOverlayRenderer.enabled = true;
         if (cast < 0.3f)
         {
-            Controller.instance.bossOverlayRenderer.color = purple;
+            Controller.instance.bossOverlayRenderer.color = Color.yellow;
             animator.SetTrigger("Wave");
         }
         else if (cast < 0.8f)
